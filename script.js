70e60a01e1b606db1b408b9846790b527be60382
moostofa <mmdrahinz96@gmail.com>
2021-11-17 11:13:29 +1100

    created color-changing effect on mouseover event

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
+    generateGrid()
+})
+
+function generateGrid() {
+    // default: generate a 4x4 grid
     for (let i = 0; i < 10; i++) {
         const row = document.createElement("div")
         row.classList = "row"
         for (let j = 0; j < 10; j++) {
             const col = document.createElement("div")
-            col.innerHTML = "hello"
+            col.innerHTML = "?"
             col.classList = "col"
+    
+            // if a mouse is hovered over a square box, change its background color to a random RGB value
+            col.addEventListener("mouseover", () => {
+                col.style.backgroundColor = `rgba(${Array.from({length:3}, () =>
+                    Math.floor(Math.random() * 256)).join(",")}, ${0.3})`
+            })
             row.append(col)
         }
         document.querySelector(".container").append(row)
     }
-})
+
+}
